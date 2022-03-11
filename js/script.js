85c39f290c956880a9026dda9bb2f1eedd2b0693
marco-cuconato <99355087+marco-cuconato@users.noreply.github.com>
2022-03-11 18:27:22 +0000

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Created by: marco cuconato 
+// Created on: Mar 2022
+// This file contains the JS functions for index.html
+
+/**
+ * Do basic math.
+ */
+
+function doMathClicked() {
+  
+document.getElementById('area').innerHTML = '<p>5 × 3 = ' + (5 * 3) + 'cm²</p>'
+document.getElememtById('perimeter').innerHTML = '<p>2(5 + 3) = ' + (2 * (5 + 3) 'cm</p>'
+}
